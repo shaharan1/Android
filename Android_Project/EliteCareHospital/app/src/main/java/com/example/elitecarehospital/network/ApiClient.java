fe3921127e06d56930f49ae475b8f8f93f3e5d88
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-08-11 22:03:23 +0600

    Add

--- Android_Project/EliteCareHospital/app/src/main/java/com/example/elitecarehospital/network/ApiClient.java
+++ Android_Project/EliteCareHospital/app/src/main/java/com/example/elitecarehospital/network/ApiClient.java
@@ -16,9 +16,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     // Emulator
-    private static final String BASE_URL = "http://10.0.2.2:8085/";
+//    private static final String BASE_URL = "http://10.0.2.2:8085/";
 
-//    public static final String BASE_URL = "http://192.168.0.103:8085/";
+    public static final String BASE_URL = "http://192.168.0.103:8085/";
 
     private static ApiService apiService;
 
